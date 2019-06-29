a0586fdba81698dfca5cb877544abb22a8d29ebe
MarinaGabarasvili <marina.gabarasvili@gmail.com>
2019-06-29 16:24:34 +0300

    Homework

--- Day1/src/day1/Day1.java
+++ Day1/src/day1/Day1.java
@@ -17,6 +17,24 @@ public class Day1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+              sum(2,3);
+              
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Ievadiet pirmo skaitli: ");
+    int x = sc.nextInt();
+    System.out.println("Ievadiet otro skaitli: ");
+    int y = sc.nextInt();
+    System.out.println("Kadu darbibu veikt: + vai -");
+    String count = sc.next();
+    sumOrSubstract(x,y,count);
+   
+    }
+    
+            
+    private static void sum (int a, int b){
+        System.out.println(a+b);
+}
+    private static void example(){
         System.out.print("Hello world2!");//bez enter
         System.out.println("Hello world!");// ar enter
         
@@ -40,9 +58,13 @@ public class Day1 {
          if(text.equals("vii")){
          //if stringam Ja van vienads, tad prieksaa liekam !
          }
-         
-         
-         
     }
+    private static void sumOrSubstract(int x, int y, String count){
     
-}
+    if(count.equals("+"))
+    {System.out.println(x+y);} 
+    else if(count.equals("-"))
+    {System.out.println(x-y);}
+    else{System.out.println("Kluda!");}
+    
+}}
