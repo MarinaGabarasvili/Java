85725fb5b83e402cc5deea7af76882e4e64dec1c
MarinaGabarasvili <marina.gabarasvili@gmail.com>
2019-07-05 11:31:35 +0300

    Calculator Excercises Added

---
+++ Day6/src/day6/CalculatorActions.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day6;
+
+/**
+ *
+ * @author jlapi
+ */
+public class CalculatorActions {
+    public static int Add(int number1, int number2){
+        return number1 + number2;
+    }
+    
+    public static int Substract(int number1, int number2){
+        return number1 - number2;
+    }
+    
+    public static int Multiply (int number1, int number2){
+        return number1 * number2;
+    }
+    
+    public static double Devide(int number1, int number2){
+        return number1 / number2;
+    }
+}
---
+++ Day6/src/day6/Day6.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day6;
+
+/**
+ *
+ * @author jlapi
+ */
+public class Day6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        TestFrame fr = new TestFrame();
+//        fr.setVisible(true);
+
+//        Vards name = new Vards();
+//        name.setVisible(true);
+        
+        Calculator cr = new Calculator();
+        cr.setVisible(true);
+        
+//        SimpleCalculator sc = new SimpleCalculator();
+//        sc.setVisible(true);
+        
+    }
+
+}
